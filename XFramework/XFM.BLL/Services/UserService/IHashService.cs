14e3b5180aabc824c7b8e0fba95c47c4aa892759
mcyasar <muratcany0167@gmail.com>
2025-08-05 12:14:58 +0300

    Hash servisin yazılması

---
+++ XFramework/XFM.BLL/Services/UserService/IHashService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFM.BLL.Services.UserService
+{
+    public interface IHashService
+    {
+        string HashPassword(string plainedPassword);
+        string VerifyPassword(string hashedPassword, string plainedPassword);
+    }
+}
--- XFramework/XFramework/Program.cs
+++ XFramework/XFramework/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using XFM.BLL.Services.UserService;
 using XFM.DAL;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IHashService, HashService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
