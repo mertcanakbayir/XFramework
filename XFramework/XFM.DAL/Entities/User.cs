8f336ef36863d51b13a2eccc53e0b5316aa6cdbe
mertcanakbayir <mertcanakbayir40@gmail.com>
2025-08-05 11:38:08 +0300

    generic repo eklendi

---
+++ XFramework/XFM.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace XFM.DAL.Entities
+{
+    public class User:BaseEntity
+    {
+        public string Username { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
