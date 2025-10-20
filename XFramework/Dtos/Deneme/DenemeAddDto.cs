d23e99ab44ee22e871b3b9e7f0204dca95548f45
mertcanakbayir40 <mertcanakbayir40@gmail.com>
2025-10-20 14:55:36 +0000

    Deleted Dtos

--- XFramework/Dtos/Deneme/DenemeAddDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace XFramework.Dtos.Deneme
-{
-    public class DenemeAddDto
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-}
--- XFramework/Dtos/Kerem/KeremAddDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace XFramework.Dtos.Kerem
-{
-    public class KeremAddDto
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-}
